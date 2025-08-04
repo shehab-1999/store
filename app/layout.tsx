871bfd7ef61d3467a62aa4cd6cfbbd3e3dfc2c9f
shehab/1999 <alaamerishehab@gmail.com>
2025-08-04 22:57:33 +0300

    9

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,14 @@ import { ThemeProvider } from '@/components/theme-provider';
 
 
 
-export const metadata: Metadata = {
+export async function generateMetadata():Promise<Metadata | undefined>{
+
+   
+  return {
+   
   title: 'متجر الأناقة - أحدث صيحات الموضة',
   description: 'اكتشف مجموعة حصرية من الملابس العصرية للرجال والنساء مع أفضل الأحذية والحقائب',
+  
    openGraph: {
     title: 'متجر الأناقة - أحدث صيحات الموضة',
   description: 'اكتشف مجموعة حصرية من الملابس العصرية للرجال والنساء مع أفضل الأحذية والحقائب',
@@ -16,13 +21,14 @@ export const metadata: Metadata = {
       url: "https://store-silk-three.vercel.app/",
       siteName: "مستشفى المجد",
       images:[{
-        url:"https://images.pexels.com/photos/769749/pexels-photo-769749.jpeg?auto=compress&cs=tinysrgb&w=500",
+        url:"https://store-silk-three.vercel.app/opengraph-image.jpg",
         width: 1200,
           height: 630,}
       ]
     }
-  
-};
+   
+  };
+}
 
 export default function RootLayout({
   children,
